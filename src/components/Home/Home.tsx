cebfe8f15e97936239b4d0149435c86993b8783e
Jonathan <69825901+edobxt@users.noreply.github.com>
2021-07-30 00:51:10 -0400

    add: typing effect sur la page d'accueil

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,8 +1,27 @@
 import * as React from "react";
 import "./Home.css";
+import ReactTypingEffect from "react-typing-effect";
 
 interface Props {}
 
 export const Home: React.FC<Props> = () => {
-    return <div>Welcome to my website ! 🚀</div>;
+    const textToType: string[] = ["developer", "student"];
+
+    return (
+        <div>
+            <div>Welcome to my website ! 🚀</div>
+            <br />
+            <div>
+                <p className="text-2xl">
+                    Hi 👋 I'm Jonathan, a{" "}
+                    <ReactTypingEffect
+                        text={textToType}
+                        speed={100}
+                        typingDelay={1000}
+                        eraseDelay={2000}
+                    />
+                </p>
+            </div>
+        </div>
+    );
 };
