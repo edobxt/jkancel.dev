ce590e382ba40b38757578304af3fb1836357f76
Jonathan <69825901+edobxt@users.noreply.github.com>
2021-08-02 09:33:36 -0400

    add: Creation du component InfoBar

---
+++ src/components/InfoBar/InfoBar.tsx
@@ -0,0 +1,29 @@
+import * as React from "react";
+import "./InfoBar.css";
+
+interface Props {
+    title: string;
+    type: string;
+}
+
+export const InfoBar: React.FC<Props> = (props) => {
+    let bgColor: string = "";
+    let txtColor: string = "";
+
+    switch (props.type) {
+        case "dev":
+            bgColor = "#fefefe";
+            txtColor = "#000";
+    }
+
+    const styles = {
+        backgroundColor: bgColor,
+        color: txtColor,
+    };
+
+    return (
+        <div className="info-bar" style={styles}>
+            <p>{props.title}</p>
+        </div>
+    );
+};
