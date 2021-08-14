7597288dc49da916ab74e3731f4f4b33bdbe62c7
Jonathan <69825901+edobxt@users.noreply.github.com>
2021-08-14 15:15:19 -0400

    add: contenu de base des pages

--- src/components/ProjectDetails/ProjectDetails.tsx
+++ src/components/ProjectDetails/ProjectDetails.tsx
@@ -1,26 +1,65 @@
 import * as React from "react";
+import "./ProjectDetails.css";
 import { useLocation } from "react-router";
 import projectsDetails from "../../utils/projectsDetails";
+import { Link } from "react-router-dom";
 
 interface Props {}
 
 export const ProjectDetails: React.FC<Props> = (props) => {
+    // Type d'un projet
+    type Project = {
+        title: string;
+        date: number | string;
+        url: string;
+        githubLink: string;
+        description: string;
+        logo?: string;
+    };
+
     // Récupérer la route actuelle
     const location = useLocation();
     const route = location.pathname;
     // Récupérer uniquement le nom de la page
     const page = route.split("/")[2];
 
-    let title, date;
+    let project: Project = {
+        title: "undefined",
+        date: "undefined",
+        url: "#",
+        githubLink: "#",
+        description: "undefined",
+    };
+
     switch (page) {
         case "ogites":
-            title = projectsDetails.ogites.title;
-            date = projectsDetails.ogites.date;
+            project = projectsDetails.ogites;
             break;
 
         case "geoworld":
-            title = projectsDetails.geoworld.title;
-            date = projectsDetails.geoworld.date;
+            project = projectsDetails.geoworld;
+            break;
     }
-    return <div>{`Project ${title} created in ${date}`}</div>;
+    return (
+        <div>
+            <Link to="/projects">
+                <button>Retour</button>
+            </Link>
+            <div className="grid grid-cols-2">
+                <div className="project-title items-center">
+                    <h1>TITLE : {project.title.toUpperCase()}</h1>
+                    <div className="project-date">{project.date}</div>
+                </div>
+                <div className="project-image">
+                    <img src={project.logo} alt="project-logo" />
+                </div>
+            </div>
+            <div className="links grid grid-cols-2">
+                <a href={project.url}>Checkout this application</a>
+                <a href={project.githubLink}>There's the github page</a>
+            </div>
+            <br />
+            <div className="description">{project.description}</div>
+        </div>
+    );
 };
--- src/utils/projectsDetails.ts
+++ src/utils/projectsDetails.ts
@@ -1,11 +1,23 @@
+//Logo ogites
+import ogitesLogo from "../assets/ogites/ogites-logo.png";
+import geoworldLogo from "../assets/geoworld/geoworld-logo.png";
+
 const projectDetails = {
     ogites: {
         title: "ogites",
         date: 2020,
+        url: "http://34.75.58.178/ogites2/index.php",
+        githubLink: "https://github.com/ogites/ogites2",
+        description: "Booking app",
+        logo: ogitesLogo,
     },
     geoworld: {
         title: "geoworld",
         date: 2021,
+        url: "http://34.75.58.178/geoworld/index.php",
+        githubLink: "https://github.com/edobxt/ppe3-geoworld",
+        description: "Geopolitical info app",
+        logo: geoworldLogo,
     },
 };
 
